a71310b78a9fbc36e8ebcfc60694ba34d0aa4ffe
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-06-14 16:04:40 +0900

    feat: add sample code

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ launchの仕様確認など、実験に用いるパッケージ
 <https://akirayou.net/wp/2020/ros2-%E3%81%AElaunchconfiguration/>
 <https://zenn.dev/uedake/articles/ros2_launch1_basic>
 <https://answers.ros.org/question/390767/ros2-multi-robot-launch-files-with-variable-parameters/>
+<https://answers.ros.org/question/396345/ros2-launch-file-how-to-convert-launchargument-to-string/>
--- test/test_ros_func.py
+++ test/test_ros_func.py
@@ -14,8 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# https://answers.ros.org/question/396345/ros2-launch-file-how-to-convert-launchargument-to-string/
 
-from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
 
-target_dir = get_package_share_directory("talker_listener")
-print(target_dir)
+from launch import LaunchContext, LaunchDescription, LaunchService
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+
+robot_description_path = Path("destination.urdf")
+
+
+def render_xacro(context: LaunchContext, *args, **kwargs):
+    support_package_str = context.perform_substitution(args[0])
+    # render xacro... just dumping the support_package value in there for example.
+    robot_description_config = support_package_str
+    robot_description_path.write_text(robot_description_config)
+    print(f"wrote robot description to {robot_description_path}")
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("support_package", default_value="hello"),
+            OpaqueFunction(
+                function=render_xacro, args=[LaunchConfiguration("support_package")]
+            ),
+            ExecuteProcess(cmd=["cat", str(robot_description_path)], output="screen"),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    ls = LaunchService()
+    ls.include_launch_description(generate_launch_description())
+    ls.run()
